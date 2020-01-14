ec820e3ebc3dce4527bb9a8e55dfe1fb29a715bd
naz_liton <nazrulliton@icloud.com>
2020-01-14 21:04:16 +0000

    test passed for empty array list in vistory

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -28,4 +28,9 @@ public class VisitorTest {
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
     }
+
+    @Test
+    public void vistedAttractiosStartsEmpty(){
+        assertEquals(0, visitor.getVisitedAttractions().size());
+    }
 }
