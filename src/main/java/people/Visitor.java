0eac3a52f00c2fbc8dda4e3e4cd0ad94f57ab5cc
naz_liton <nazrulliton@icloud.com>
2020-01-14 21:02:37 +0000

    arraylist added to visitor

--- src/main/java/people/Visitor.java
+++ src/main/java/people/Visitor.java
@@ -1,15 +1,21 @@
 package people;
 
+import attractions.Attraction;
+
+import java.util.ArrayList;
+
 public class Visitor {
 
     private int age;
     private double height;
     private double money;
+    private ArrayList<Attraction> visitedAttractions;
 
     public Visitor(int age, double height, double money) {
         this.age = age;
         this.height = height;
         this.money = money;
+        visitedAttractions = new ArrayList<>();
     }
 
     public int getAge() {
@@ -22,5 +28,9 @@ public class Visitor {
 
     public double getMoney() {
         return money;
+    }
+
+    public ArrayList<Attraction> getVisitedAttractions() {
+        return visitedAttractions;
     }
 }
