ec7ffcf31f901463b8b7f80cbcbd5709759ef94a
naz_liton <nazrulliton@icloud.com>
2020-01-14 19:42:10 +0000

    themepark funcs created, tests to follow

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,15 @@
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+    private ArrayList<IReviewed> items;
+
+    public ThemePark(){
+        this.items = new ArrayList<IReviewed>();
+    }
+    public int itemCount(){
+     return items.size();
+    }
+
 }
